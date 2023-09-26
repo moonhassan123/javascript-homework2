e77d03a3f4bfd6190c85e906aaa3068bcae74a27
moonhassan123 <moonhassan9899@gmail.com>
2023-09-26 15:29:08 +0600

    complet

---
+++ demo.js
@@ -0,0 +1,4 @@
+let stedentAge = [12,13,14,15];
+console.log(stedentAge[0]);
+console.log(stedentAge[1]);
+console.log(stedentAge[2]);
--- forloop.js
+++ forloop.js
@@ -1,5 +1,9 @@
-// for ( let i = 10; i > 0; i--){
-//     console.log(i);
+// for ( let i = 1; i <= 10; i+=2){
+    // console.log("2 X", i, " = ", 2*i);
+// console.log(2*i);
+// console.log(-i);
+// console.log(3 * i);
+// console.log(i);
 
 // }
     
@@ -8,18 +12,32 @@
 // // }
 
 // for (let i = 1; i <= 10; i++) {
-//     // console.log(`${2} * ${i} = ${2 * i}`);
-//     console.log( "3 X ", i, " = ", 3*i )
+//     console.log(`${2} * ${i} = ${2 * i}`);
+// //     console.log( "3 X ", i, " = ", 3*i )
 //   }
 //   console.log(['jdnjsfu','dsbcbschj',]);
 
 
-for (let i = 10; i >= 0; i--){
-    console.log("3 X", i, " = ", 3*i);
-    console.log("4 X", i, " = ", 4*i);
-    console.log("5 X", i, " = ", 5*i);
-    console.log("6 X", i, " = ", 6*i);
+// for (let i = 1; i <= 10; i++){
+//     console.log("3 X", i, " = ", 3*i);
+    // console.log("4 X", i, " = ", 4*i);
+    // console.log("5 X", i, " = ", 5*i);
+    // console.log("6 X", i, " = ", 6*i);
+// }
 
+for (let i = 1; i <= 4; i++){
+
+    for (let j = 1; j <= 10; j++){
+        // console.log(i, 'x', j, '=', (i * j));
+    }
 }
-
-
+for (let i = 1; i <= 10; i++){
+   
+    // if (i == 10){
+        // break;
+        if (i < 10){
+            continue;
+        }
+        console.log(i);
+    // }
+}
